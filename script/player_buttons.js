d5b0f97fbd541e0003589af1f9a53b2ad6aa74a0
Your Name <abdalshafiea@gmail.com>
2021-05-08 13:41:06 -0700

    update

---
+++ script/player_buttons.js
@@ -0,0 +1,83 @@
+class Player{
+    constructor(){
+        
+        this.audio_file = document.getElementById("audio_file");
+        this.isPlayed;
+
+        this.play = document.getElementById("play");
+        this.play.addEventListener("click",()=>{this.play_pause();});
+
+        this.audios =[];
+        this.audios[0] = "https://stream.live.vc.bbcmedia.co.uk/bbc_arabic_radio";
+        this.audios[1] = "./audio/music.mp3";
+        this.audios[2] = "http://n04.radiojar.com/yx0q47hna3quv?rj-ttl=5&rj-tok=AAABeU20wpYA7mgh1wztQk-Bzw";
+    
+        this.names = [];
+        this.names[0] = "Radio ليبيا ";
+        this.names[1] = "Radio 2";
+        this.names[2] = "Radio سوا ليبيا";
+        
+        this.numberAudio =0;
+
+     
+
+        this.next = document.getElementById("next");
+        this.next.addEventListener("click",()=>{
+            this.next_audio();
+        });
+           
+
+        this.back = document.getElementById("back");
+        this.back.addEventListener("click",()=>{
+            
+          this.back_audio();  
+         
+        });
+
+        this.setSource();
+    }
+    play_pause(){
+        if(this.isPlayed==false){
+            this.play.src = "./img/pause.png";
+            this.audio_file.play();
+            this.isPlayed=true;
+        }
+        else{
+            this.play.src = "./img/play.png";
+            this.audio_file.pause();
+            this.isPlayed=false;
+        }
+    }
+
+    next_audio(){
+         if(this.numberAudio<this.audios.length-1){
+                ++this.numberAudio;
+                this.isPlayed = false; 
+                }
+         else   {
+                this.numberAudio = 0;
+                }
+                this.setSource();
+    }
+    back_audio(){
+        if(this.numberAudio>0){
+            --this.numberAudio;
+            this.isPlayed = false; 
+         }
+         else{
+            this.numberAudio = this.audios.length -1;
+            
+         }
+      
+         this.setSource();
+    }
+    setSource(){
+ 
+    document.getElementById("radio-title").innerHTML = this.names[this.numberAudio];
+    document.getElementById("audio_file").src = this.audios[this.numberAudio];
+
+    this.play_pause();
+    }
+
+}
+onload = new Player();
